2f3499c63fceaa408ed19c9e68537959383e595e
Loay Gen <genloay@gmail.com>
2025-11-13 13:27:20 +0300

    5

--- src/components/MainForm.jsx
+++ src/components/MainForm.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-
+import lo from "../../public/lo.jpg";
 export default function MainForm({ onNext }) {
   const [formData, setFormData] = useState({
     name: "",
@@ -68,7 +68,7 @@ export default function MainForm({ onNext }) {
         {/* الرأسية */}
         <div className="text-center mb-6 sm:mb-10">
           <div className="inline-flex items-center justify-center   sm:w-20 sm:h-20 bg-gradient-to-br from-slate-800 to-slate-900 rounded-2xl mb-4 shadow-lg">
-             <img className="rounded-xl" src="../../public/lo.jpg" alt="" />
+             <img className="rounded-xl" src={lo} alt="" />
           </div>
           <h1 className="text-2xl sm:text-3xl font-bold text-slate-900 mb-2">
             استمارة التقديم على الوظائف
